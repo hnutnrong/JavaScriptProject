6765351228e00e32104c89617ca214dcca812682
Nutnarong <35445939+hnutnrong@users.noreply.github.com>
2020-03-15 21:22:17 +0700

    [Add]Arrow function & Default Parameter

---
+++ ES6Tutorial/arrowfunction.js
@@ -0,0 +1,20 @@
+// const sorwor = a => a+200
+
+// let result = sorwor(50)
+// console.log(result)
+
+
+const student=name =>({name:name,age:10})
+let result=student("nutnarong")
+console.log(result);
+//มาจาก 
+// function student(name){
+//     user={name:'',age:}
+//     return ............
+// }
+
+//มีปีกกา กรณีที่มีคำสั่งมากกว่า 1
+//มีการ return 
+// return object
+//ไม่มีปีกกา
+// parameter 1 ตัว ไม่ต้องมีวงเล็บ
---
+++ ES6Tutorial/defaultparameter.js
@@ -0,0 +1,8 @@
+//Default Parameter
+
+const addUser=(name,last,city="Ratchaburi")=>{
+    return name + last + "city = " + city
+}
+console.log(addUser("nutnarong","yookittichai","Bangkok"));
+console.log(addUser("puimek","yookittichai")) ;
+//ถ้ากำหนดไม่ครบมันก็จะเป็น undefied แต่ถ้าเรากำหนด city ตั้งแต่แรกมันก็จะเป็นค่านั้น(Ratchaburi)

58da13c6541ee4f9f55d66732492efae4c2d11c1
Nutnarong <35445939+hnutnrong@users.noreply.github.com>
2020-03-15 23:00:42 +0700

    [Add]Computed Property Name

--- ES6Tutorial/computedpropertyname.js
+++ ES6Tutorial/computedpropertyname.js
@@ -1,1 +1,30 @@
 //Compute Property Name
+//การดำเนินการภายใน Properties
+let food1={name:"Cake",price:"200"}
+let food2={"name":"Cake","price":250}
+
+console.log(food1)
+console.log(food2)
+
+let user={
+    ['user'+1]:'nut',
+    ['user'+2*10]:'aaa'
+}
+console.log(user);
+
+
+///การเอาตัวแปรจากด้านนอกมาประกาสเป็น Properties ด้านใน
+let name = 'Cake'
+let menu ={
+    [name]:'Orange Cake', ///เติม []เข้าไป มันก็จะกลายเป็นคำว่า Cake จากตัวแปรด้านบน
+    [name+'Mix']:'orange',
+    price:'250'
+}
+
+//การเขียนแบบ Destructuring
+//การดึงค่าจาก propeties มากำหนดเป็นตัวแปร
+let {Cake,CakeMix}=menu
+console.log(Cake)
+console.log(CakeMix)
+
+

a6fd065b55721e08d5235e7d574dc3dedb36b8e9
Nutnarong <35445939+hnutnrong@users.noreply.github.com>
2020-03-15 12:04:30 +0700

    [Add] ใช้ let ในการสร้างตัวแปร แทน var
    ปัจจุบันใช้ let ในการสร้างตัวแปร แทน var และ javascript ปัจจุบันไม่ต้องใส่ ; ก็ได้

--- BasicJavaScript/app2.js
+++ BasicJavaScript/app2.js
@@ -1,9 +1,9 @@
 
 
-var randomNumber = Math.floor(Math.random() * 5)+1 ; 
+let randomNumber = Math.floor(Math.random() * 5)+1 ; 
 ///Math.random มันจะ random แค่ 0-1 ถ้าอยากได้เลขถึงเท่าไหร่ก็เอาไปคูณเช่น 5
 /// Math.floor เพื่อทำให้เป็นจำนวนเต็ม
-var input = prompt ('Choose your Number 1-5');
+let input = prompt ('Choose your Number 1-5');
 //prompt เป็นคำสั่งรับค่าจากผู้ใช้
 
 if(parseInt(input) === randomNumber){ ///parseInt เพราะค่าที่รับมามันเป็นString
--- BasicJavaScript/function2.js
+++ BasicJavaScript/function2.js
@@ -1,10 +1,10 @@
-var msg = 'hello';
+var msg = 'hello'
 
 function showMsg(){
-    var msg = 'good bye';     ///ถ้าไม่ใส่ var มันก็จะโชว์เป็นตัว global คือ hello
-    document.write(msg);
+    let msg = 'good bye'    ///ถ้าไม่ใส่ var มันก็จะโชว์เป็นตัว global คือ hello
+    document.write(msg)     ///ปัจจุบันใช้ เป็น let ใสการสร้างตัวแปร
 }
 
 showMsg(); ///จะ show msg ใน function
 
-document.write(msg); //มันก็จะ show hello ที่เป็นตัว global 
+document.write(msg) //มันก็จะ show hello ที่เป็นตัว global 
